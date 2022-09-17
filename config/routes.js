fe2e1d57d1aa549523da3a68af56de92d610bc25
HenriqueSantos2001 <103902026+HenriqueSantos2001@users.noreply.github.com>
2022-09-17 05:13:42 -0300

    feat: delete

--- config/routes.js
+++ config/routes.js
@@ -2,16 +2,16 @@ const express = require("express");
 const routes = express.Router();
 
 let dummyData = [
-    { "Cliente1": { Name: "Joao", Age: "20" } },
-    { "Cliente2": { Name: "Jose", Age: "18" } },
-    { "Cliente3": { Name: "Maria", Age: "22" } },
+    { "1": { Name: "Joao", Age: "20" } },
+    { "2": { Name: "Jose", Age: "18" } },
+    { "3": { Name: "Maria", Age: "22" } },
 ];
 
-app.get("/", (req, res) => {
+routes.get("/", (req, res) => {
     return res.json(dummyData)
 });
 
-app.post("/add", (req, res) => {
+routes.post("/add", (req, res) => {
     const body = req.body;
 
     if(!body)
@@ -20,6 +20,19 @@ app.post("/add", (req, res) => {
     dummyData.push(body);
 
     return res.json(body);
+});
+
+routes.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    let newDB = dummyData.filter(item => {
+        if(!item[id])
+            return item;
+    });
+
+    dummyData = newDB;
+
+    return res.send(newDB);
 });
 
 module.exports = routes;
