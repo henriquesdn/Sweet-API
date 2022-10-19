b3871bd154497231c02136931e8630194c7d528b
HenriqueSantos2001 <103902026+HenriqueSantos2001@users.noreply.github.com>
2022-10-19 13:10:03 -0300

    feat: eslint and prettier implementation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require("./config/routes");
 const app = express();
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(routes);
---
+++ src/routes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const routes = express.Router();
+
+let dummyData = [
+    { 1: { Name: "Joao", Age: "20" } },
+    { 2: { Name: "Jose", Age: "18" } },
+    { 3: { Name: "Maria", Age: "22" } },
+];
+
+routes.get("/", (req, res) => res.json(dummyData));
+
+routes.post("/add", (req, res) => {
+    const { body } = req;
+
+    if (!body) return res.status(400).end();
+
+    dummyData.push(body);
+
+    return res.json(body);
+});
+
+routes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const newDB = dummyData.filter((item) => {
+        if (!item[id]) return item;
+    });
+
+    dummyData = newDB;
+
+    return res.send(newDB);
+});
+
+module.exports = routes;
